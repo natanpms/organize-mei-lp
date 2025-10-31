6a63a3ec60c6ffa184c69ff09518ba5f796ff6bd
Natan Porto Machado da Silva <natanportoms@outlook.com>
2025-10-31 13:04:25 -0300

    fix: ajuste padding sections

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import organizeMei from "./assets/organize-mei.jpg";
 function App() {
   return (
     <div className="flex flex-col w-full min-h-screen">
-      <section className="container mx-auto py-2 w-full">
+      <section className="container mx-auto py-6 md:py-12 w-full">
         <div className="flex flex-col justify-center items-center px-6 gap-6">
           <h2 className="text-2xl font-bold text-[#252b42]">Por que usar OrganizeMEI?</h2>
           <div className="w-full">
@@ -23,12 +23,12 @@ function App() {
       </section>
 
       {/* seção iphone */}
-      <section className="flex flex-col lg:flex-row items-center justify-around w-full container mx-auto py-4 md:py-12 flex-1">
+      <section className="flex flex-col lg:flex-row items-center justify-around w-full container mx-auto py-6 md:py-12 flex-1">
         <div className="text-center md:text-left space-y-4 max-w-xl">
           <h1 className="text-3xl lg:text-5xl font-bold text-[#252b42]">
             Pronto para iniciar?
           </h1>
-          <p className="text-lg font-medium text-gray-500">
+          <p className="text-lg font-medium text-gray-500 px-2">
             Conheça o software que descomplica sua vida!
           </p>
           <a
